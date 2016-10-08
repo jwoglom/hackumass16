0ca59f479a65e11ff2be85551a7cf6bd2aab6af8
adunna <adunna@localhost.localdomain>
2016-10-08 15:58:11 -0400

    Update for threading & calc

---
+++ hackum/apps/core/jqueue.py
@@ -0,0 +1,11 @@
+class JQueue:
+    def __init__(self):
+        self.queue = []
+    def push(self, element):
+        self.queue.append(element)
+    def pop(self):
+        self.queue.pop(0)
+    def size(self):
+        return len(self.queue)
+    def toList(self):
+        return self.queue;
--- hackum/apps/core/views.py
+++ hackum/apps/core/views.py
@@ -4,8 +4,18 @@ from django.http import JsonResponse, HttpResponse
 
 import time
 import tempfile
+import json
+import numpy as np
+from threading import Thread
 from binascii import a2b_base64
 from clarifai.client import ClarifaiApi
+from .jqueue import JQueue
+
+global queue
+global statResult
+
+queue = JQueue()
+statResult = JQueue()
 
 def index_view(request):
     return render(request, "index.html", {})
@@ -15,7 +25,6 @@ def vidtest_view(request):
 
 def api_view(request):
     #return HttpResponse("""{"results": [{"local_id": "", "docid_str": "8317466b40dd933e9eb8c72e5ac0bc5b", "docid": 174249718541730833606777867238536952923, "result": {"tag": {"concept_ids": ["ai_Pf2b7clG", "ai_6lhccv44", "ai_dxSG2s86", "ai_TJ9wFfK5", "ai_bBXFkGB1", "ai_VPmHr5bm", "ai_l8TKp2h5", "ai_7WNVdPhm", "ai_86sS08Pw", "ai_8H8gb4Z2", "ai_SVshtN54", "ai_Hq72S9X5", "ai_62K34TR4", "ai_p9bzR7fH", "ai_dngMN46t", "ai_ggQlMG6W", "ai_GkhKcPVj", "ai_2D3NBTV7", "ai_fKppxwrj", "ai_971KsJkn"], "classes": ["indoors", "business", "man", "portrait", "auto racing", "adult", "people", "competition", "wear", "serious", "one", "election", "technology", "education", "fashion", "industry", "airport", "championship", "soccer", "track"], "probs": [0.9916660785675049, 0.970708966255188, 0.9623082876205444, 0.9480817317962646, 0.9227888584136963, 0.9192773699760437, 0.9189565181732178, 0.8990598320960999, 0.8369446992874146, 0.831376314163208, 0.828432559967041, 0.8234387040138245, 0.81510329246521, 0.8128798007965088, 0.8095601797103882, 0.7979507446289062, 0.7976885437965393, 0.7952430248260498, 0.7913222908973694, 0.7839663028717041]}}, "status_code": "OK", "status_msg": "OK"}], "status_msg": "All images in request have completed successfully. ", "meta": {"tag": {"model": "general-v1.3", "config": null, "timestamp": 1475940923.318772}}, "status_code": "OK"}""", content_type='text/json')
-
     if request.GET.get("time"):
         return HttpResponse(time.time())
 
@@ -39,6 +48,8 @@ def api_view(request):
             # clarifai_api.tag_image_urls(url)
             print(time.time(), "sendAPI")
             json_resp = clarifai_api.tag(file)
+            queue.push(json_resp)
+            print(queue.size())
             print(time.time(), "doneAPI")
 
             t1 = time.time()
@@ -58,3 +69,39 @@ def process_file_b64(b64):
     print(time.time(), "doneAPI")
 
     return json_resp
+
+# json object -> Iterator[Tag, Prob]
+def getKeywords(data):
+    status = data["results"][0]["status_code"]
+    if (status == 'OK'):
+        tags = data["results"][0]["result"]["tag"]
+        return (set(tags["classes"]), dict(zip(tags["classes"], tags["probs"])))
+    else:
+        print("Warning: Status was not OK")
+        return iter(())
+
+# List[Json] -> Map[Key, (Mean, Std)]
+def calculate(queue):
+    eps = 5;
+    intersectKeys = {} # This is incorrect
+    if(queue.size() > eps):
+        mapped = map(getKeywords,queue.toList())
+        intersectKeys = reduce(lambda x, y: (x[0] & y[0]), mapped)
+        probsDict = map(lambda k: (k,np.array([l[1][k] for l in mapped])), intersectKeys)
+        queue.pop()
+        return dict(map(lambda k: (k[0], (np.mean(k[1]), np.std(k[1]))),probsDict))
+    else:
+        return None
+
+def top():
+    global queue
+    global statResult
+    while(True):
+        res = calculate(queue)
+        if(res != None):
+            statResult.push(res)
+            print("pushed")
+        time.sleep(0.05)
+
+calcThread = Thread(target=top)
+calcThread.start()
