45aab0feab4bdbe3f53d5b9f1a004cd7432e4389
James Woglom <j@wogloms.net>
2016-10-08 14:34:37 -0400

    use websocket; currently not working

--- hackum/apps/core/consumers.py
+++ hackum/apps/core/consumers.py
@@ -1,4 +1,5 @@
 import tempfile
+import json
 from binascii import a2b_base64
 
 from channels import Group
@@ -18,15 +19,20 @@ def http_consumer(message):
 @channel_session
 def ws_connect(message):
     prefix, label = message['path'].strip('/').split('/')
-    message.channel_session['room'] = label    
+    message.channel_session['room'] = label
+    print("WSconnect", label)
     Group('chat-'+label, channel_layer=message.channel_layer).add(message.reply_channel)
 
 @channel_session
 def ws_receive(message):
     label = message.channel_session['room']
-    if 'b64' in message:
-        b64_data = message['b64']
+    print("WSreceive", label, message['text'][:100])
+    js = json.loads(message['text'])
+    print(len(js['b64']))
+    if 'b64' in js:
+        b64_data = js['b64']
         bin_data = a2b_base64(b64_data)
+        open("/tmp/bin", "wb").write(bin_data)
         file = tempfile.TemporaryFile()
         file.write(bin_data)
         resp = process_file(file)
@@ -35,4 +41,5 @@ def ws_receive(message):
 @channel_session
 def ws_disconnect(message):
     label = message.channel_session['room']
+    print("WSdisc", label)
     Group('chat-'+label, channel_layer=message.channel_layer).discard(message.reply_channel)
