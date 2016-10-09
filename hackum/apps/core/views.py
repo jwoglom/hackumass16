d0319a2d51447a32b6d26b6e2b3e37b998c747c1
Rachit Nigam <rachit.nigam12@gmail.com>
2016-10-09 06:23:32 -0400

    Code cleaning.

--- hackum/apps/core/views.py
+++ hackum/apps/core/views.py
@@ -13,11 +13,15 @@ from .jqueue import JQueue
 
 from functools import reduce
 
+# Global queue to store the images that were sent to the server.
 global queue
-global statResult
-
 queue = JQueue()
+
+# Global quque to store the results returned from clarifai.
+global statResult
 statResult = JQueue()
+
+# Global variable defining the number of past frames that are considered for the statiscal analysis.
 global eps
 eps = 2
 
@@ -27,11 +31,15 @@ def index_view(request):
 def vidtest_view(request):
     return render(request, "vidtest.html", {})
 
+# Defines the request the server can handle
 def api_view(request, tags=""):
-    #return HttpResponse("""{"results": [{"local_id": "", "docid_str": "8317466b40dd933e9eb8c72e5ac0bc5b", "docid": 174249718541730833606777867238536952923, "result": {"tag": {"concept_ids": ["ai_Pf2b7clG", "ai_6lhccv44", "ai_dxSG2s86", "ai_TJ9wFfK5", "ai_bBXFkGB1", "ai_VPmHr5bm", "ai_l8TKp2h5", "ai_7WNVdPhm", "ai_86sS08Pw", "ai_8H8gb4Z2", "ai_SVshtN54", "ai_Hq72S9X5", "ai_62K34TR4", "ai_p9bzR7fH", "ai_dngMN46t", "ai_ggQlMG6W", "ai_GkhKcPVj", "ai_2D3NBTV7", "ai_fKppxwrj", "ai_971KsJkn"], "classes": ["indoors", "business", "man", "portrait", "auto racing", "adult", "people", "competition", "wear", "serious", "one", "election", "technology", "education", "fashion", "industry", "airport", "championship", "soccer", "track"], "probs": [0.9916660785675049, 0.970708966255188, 0.9623082876205444, 0.9480817317962646, 0.9227888584136963, 0.9192773699760437, 0.9189565181732178, 0.8990598320960999, 0.8369446992874146, 0.831376314163208, 0.828432559967041, 0.8234387040138245, 0.81510329246521, 0.8128798007965088, 0.8095601797103882, 0.7979507446289062, 0.7976885437965393, 0.7952430248260498, 0.7913222908973694, 0.7839663028717041]}}, "status_code": "OK", "status_msg": "OK"}], "status_msg": "All images in request have completed successfully. ", "meta": {"tag": {"model": "general-v1.3", "config": null, "timestamp": 1475940923.318772}}, "status_code": "OK"}""", content_type='text/json')
+    # Return the current time
     if request.GET.get("time"):
         return HttpResponse(time.time())
 
+    # Post an image and get clarifai tags for it.
+    # If the image is a url, download it into a temp file and then return the tags from clarifai
+    # For debugging.
     if request.method == "POST":
         file = None
         t0 = time.time()
@@ -49,7 +57,6 @@ def api_view(request, tags=""):
         if file:
             print(time.time(), "loadAPI")
             clarifai_api = ClarifaiApi()
-            # clarifai_api.tag_image_urls(url)
             print(time.time(), "sendAPI")
             if(tags == ""):
                 json_resp = clarifai_api.tag(file)
@@ -76,7 +83,9 @@ def process_file_b64(b64, classes):
     queue.push(json_resp)
     return json_resp
 
-# json object -> Iterator[Tag, Prob]
+# json object -> (Set[Tag], Dict[Tag, Prob])
+# Returns a tuple containing the set of class names and a dictionary mapping the class name
+# to the respective prob.
 def getKeywords(data):
     status = data["results"][0]["status_code"]
     if (status == 'OK'):
@@ -98,6 +107,8 @@ def calculate(queue):
     else:
         return None
 
+# Global loop for observing pushes to the queue. Whenever the the size reaches epsilon + 1,
+# the calculation is started for the last epsilon queues.
 def top():
     global queue
     global statResult
@@ -107,5 +118,6 @@ def top():
             statResult.push(res)
         time.sleep(0.05)
 
+# Start the observing thread to do the calculations.
 calcThread = Thread(target=top)
 calcThread.start()
